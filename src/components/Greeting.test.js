edc677e5c0e45b4bc14d71a02446aa69f2f4c20e
Matthew Aitken <matt@itken.net>
2025-08-26 14:24:02 +0100

    Add more tests for Greeting component

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,47 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import Greeting from "./Greeting";
 
-test('renders Hello World as text', () => {
-    // arrange
-    render(<Greeting />);
+describe('<Greeting> component', () => {
 
-    // act
-    // ...nothing
+    test('renders Hello World! as text', () => {
+        // arrange
+        render(<Greeting />);
+        // assert
+        const helloWorldElement = screen.getByText('Hello World!');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
 
-    // assert
-    const helloWorldElement = screen.getByText('Hello World!');
-    expect(helloWorldElement).toBeInTheDocument();
+    test("renders It's good to see you! if the button is NOT clicked", () => {
+        // arrange
+        render(<Greeting />);
+        // assert
+        const textElement = screen.getByText("It's good to see you!");
+        const textChangedElement = screen.queryByText("Changed!");
+        expect(textElement).toBeInTheDocument();
+        expect(textChangedElement).not.toBeInTheDocument();
+    })
+
+    test("renders Changed! if the button is IS clicked", () => {
+        // arrange
+        render(<Greeting />);
+        // act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+        // assert
+        const textElement = screen.getByText("Changed!");
+        expect(textElement).toBeInTheDocument();
+    })
+
+    test("does not render It's good to see you! if the button is IS clicked", () => {
+        // arrange
+        render(<Greeting />);
+        // act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+        // assert
+        const textElement = screen.queryByText("It's good to see you!");
+        expect(textElement).toBeNull();
+    })
+
 });
